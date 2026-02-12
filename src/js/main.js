cace0d62d58a0cb45087e04a414069bc4512252c
OksanaVS-prog <kramarenkoksana@ukr.net>
2026-02-12 20:41:21 +0200

    adaptation banner to mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 console.log('main.js подключён');
+
+const burger = document.getElementById('burger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+if (burger && mobileMenu) {
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    burger.classList.toggle('active'); // если есть анимация
+  });
+}
