c2d344711c30d3934379a8b2b77166eb6b9f7b27
OksanaVS-prog <kramarenkoksana@ukr.net>
2026-02-27 19:20:05 +0200

    create and adaptation footer and correction other blocks

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,42 @@ if (burger && mobileMenu) {
     burger.classList.toggle('active'); // если есть анимация
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const section = document.getElementById("collections");
+  if (!section) return;
+
+  const slider = section.querySelector(".collections-grid");
+  if (!slider) return;
+
+  // автоплей только на десктопе
+  if (window.innerWidth <= 1024) return;
+
+  let scrollAmount = 0;
+  const speed = 0.5;   // скорость
+  const delay = 10;    // плавность
+
+  function autoScroll() {
+    scrollAmount += speed;
+
+    if (scrollAmount >= slider.scrollWidth - slider.clientWidth) {
+      scrollAmount = 0;
+    }
+
+    slider.scrollTo({
+      left: scrollAmount,
+    });
+  }
+
+  let autoPlay = setInterval(autoScroll, delay);
+
+  slider.addEventListener("mouseenter", () => {
+    clearInterval(autoPlay);
+  });
+
+  slider.addEventListener("mouseleave", () => {
+    autoPlay = setInterval(autoScroll, delay);
+  });
+
+});
